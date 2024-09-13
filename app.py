758a65810e88bd4eb749302201be72dfaec6d788
Nathalie Estrella <nathaliebestrellamejia@gmail.com>
2024-09-12 19:49:55 -0500

    Finales, faltaba hacer push

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ import os
 app = Flask(__name__)
 
 # Cargar el modelo previamente entrenado
-model = tf.keras.models.load_model('modelo_final.h5')
+model = tf.keras.models.load_model('micorrizasmodel_nmmd.h5')
 
 # Definir las clases
 class_labels = ['ectomicorrizas', 'endomicorrizas', 'ericoide']
 
+@app.route("/", methods=["GET", "POST"])
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -22,13 +23,20 @@ def index():
             file.save(filepath)
 
             # Preprocesar la imagen
-            img = image.load_img(filepath, target_size=(150, 150))
+            img = image.load_img(filepath, target_size=(250, 250))
             img_array = image.img_to_array(img)
             img_array = np.expand_dims(img_array, axis=0)
             img_array /= 255.0
 
+            # Verificar forma de la imagen antes de la predicción
+            print(f"Forma de la imagen procesada: {img_array.shape}")
+
             # Realizar predicción
             predictions = model.predict(img_array)
+
+            # Verificar la forma de las predicciones
+            print(f"Forma de las predicciones: {predictions.shape}")
+
             predicted_class = np.argmax(predictions, axis=1)[0]
             predicted_label = class_labels[predicted_class]
             predicted_probability = np.max(predictions)
@@ -36,6 +44,7 @@ def index():
             return render_template("result.html", label=predicted_label, probability=predicted_probability)
 
     return render_template("index.html")
+
 
 @app.route('/graficos')
 def graficos():
